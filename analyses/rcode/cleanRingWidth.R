fd5ba27346135fe14a1c8f5ca8eba2d0961e2700
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2025-04-08 19:01:21 -0700

    worked on cleaning code

--- analyses/rcode/cleanRingWidth.R
+++ analyses/rcode/cleanRingWidth.R
@@ -21,5 +21,32 @@ file_list <- list.files(path = directory, pattern = "\\.csv$", full.names = TRUE
 data_list <- lapply(file_list, read.csv)
 
 # combine tables
-combined_data <- do.call(rbind, data_list)
+c <- do.call(rbind, data_list)
 
+### === === === === === ###
+# Clean labels and years #
+### === === === === === ###
+# cleaned name column
+c$Name <- gsub("_guides(_[0-9]+)?\\.tif", "", sub(":.*", "", c$Label))
+# cleaned year column
+c$Year <- sub(".*:", "", c$Label)
+# Create a new table with only year values in the Year column i.e. excluding comments
+e <- c[grepl("^\\d{2,4}$", c$Year), ] 
+# Convert year column to numeric
+e$Year <- as.numeric(e$Year)
+# Create a new column with the length in cm with a conversion factor of 2.54cm because 1 inch is 2.540005 cm
+e$LengthCM <- e$Length*2.54
+# Create a new df with only necessary columns
+d <- e[, c("Name", "Year", "LengthCM")]
+
+### Select random measurements that I will physically measure on the cookies to verify the accuracy of the measurements
+# Select 20 random measurements
+set.seed(123) # for reproducibility0
+# Select 20 random rows from the data frame
+random_rows <- sample(nrow(d), 10)
+# Create a new data frame with the selected rows  
+vec <- d$Name[random_rows]
+# Create a new data frame with the selected rows
+random_measurements <- d[vec, ]
+
+newdf <- subset(d, Name %in% vec)
