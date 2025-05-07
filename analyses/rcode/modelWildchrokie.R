df4ac463a04e81114eab21297b6b91bfbcf0ea4d
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2025-05-07 14:35:31 -0700

    worked a bunch on core verification and cleaning ring width

--- analyses/rcode/modelWildchrokie.R
+++ analyses/rcode/modelWildchrokie.R
@@ -7,7 +7,7 @@
 # housekeeping
 rm(list=ls()) 
 options(stringsAsFactors = FALSE)
-
+options(max.print = 200) 
 # Load library 
 library(rstanarm)
 library(ggplot2)
@@ -34,91 +34,75 @@ gdd <- read.csv("output/gddData.csv")
 
 ### assign more digestible names and years to help me understand what the hell im doing
 spp <- c("alninc","betpap","betpop","betall")
-tree <- c("wm1","wm2","wm3","hf1","hf2","hf3","pg1","pg2","pg3","sh1","sh2","shg3")
+plot <- c("wm1","wm2","wm3","hf1","hf2","hf3","pg1","pg2","pg3","sh1","sh2","shg3")
 yr <- 2016:2023
 rep <- 1:3
 
 Nspp <- length(spp) # number of species
-Ntree <- length(tree) # number of trees per species
+Nplot <- length(plot) # number of plot per species
 Nyr <- length(yr) # number of growth years per tree
 Nrep <- length(rep) # number of measurements per tree
 
 # First making a data frame for the growth ring data
-Nw <- Ntree*Nyr*Nrep*Nspp # number of measurements per species
+Nw <- Nplot*Nyr*Nrep*Nspp # number of measurements per species
 Nw
 
 #make a dataframe for ring width
 wdat <- data.frame(matrix(NA, Nw, 1))
 wdat$yr <- rep(c(yr), each=Nrep)
-wdat$idcookies <- rep(c(tree), each=Nyr*Nrep)
-wdat$spp <- rep(c(spp), each=Nyr*Nrep*Ntree)
-wdat
+wdat$plot <- rep(c(plot), each=Nyr*Nrep)
+wdat$spp <- rep(c(spp), each=Nyr*Nrep*Nplot)
+
+# paste plot and spp to give unique ids
+wdat$treeid <- paste(wdat$spp, wdat$plot, sep = "_")
 
 mu.grand <- 0.4 # the grand mean of ring width
 
 # now generating the year effect with data
-sigma.yr <- 0.5  # 
-mu.yr <- rnorm(Nyr, 1, sigma.yr) #intercept for each study
+sigma.yr <- 0.05  # 
+mu.yr <- rnorm(Nyr, 0, sigma.yr) #intercept for each study
 wdat$mu.yr <- rep(mu.yr, each = Nrep) # generate data for ea study
 
-# now generating the tree cookie effect
-sigma.tree <- 0.2 # setting it low for now
-mu.wtree <- rnorm(Ntree, 1, sigma.tree)
-wdat$mu.wtree <- rep(mu.wtree, each=Nyr*Nrep) # add width data for each species
-head(wdat)
+# now generating the tree cookie effect within a same species
+sigma.tree <- 0.02 # setting it low for now
+mu.tree <- rnorm(Nplot, 0, sigma.tree) 
+wdat$mu.tree <- rep(mu.tree, each=Nyr*Nrep) # add width data for each species
+wdat
 
 #now generating the species effect
-sigma.spp <- sigma.yr*2 # seting it for now as twice of the year effect 
-mu.wspp <- rnorm(Nspp, 1, sigma.spp)
-wdat$mu.wspp <- rep(mu.wspp, each=Nyr*Nrep*Ntree) # add width data for each species
-head(wdat)
-
-
+sigma.spp <- sigma.yr*1.2 # seting it for now as twice of the year effect 
+mu.spp <- rnorm(Nspp, 0, sigma.spp)
+wdat$mu.spp <- rep(mu.spp, each=Nyr*Nrep*Nplot) # add width data for each species
 
 # general variance
-w.var <- 1 #will have to change
-wdat$w.er <- rnorm(Nw, 0, w.var)
+w.var <- 0.5 #will have to change
+wdat$w.er <- rnorm(Nw, 1, w.var)
 
-# generate yhat (which is the predictive value) - width -  for this first width model
-wdat$yW <- mu.grand + wdat$mu.wsp + wdat$mu.yr + wdat$w.er
+# generate yhat aka (I guess) yhat?
+wdat$ringwidth <- mu.grand + wdat$mu.yr +wdat$mu.tree + wdat$mu.spp + wdat$w.er
+wdat
 
-## balanced subset
-wdat1 <- subset(wdat, wdat$rep <= 50)
-nrow(wdat1)
+wdat$spp <- factor(wdat$spp)
+wdat$plot  <- factor(wdat$plot)
+wdat$treeid <- factor(wdat$treeid)
+wdat$yr <- factor(wdat$yr)
 
-## Trait only stan model
-trait_data <- list(y = wdat1$yTraiti, 
-                   N = nrow(wdat1), 
-                   n_variety = Nspp, 
-                   variety = wdat1$species, 
-                   company = wdat1$study, 
-                   n_company = Nstudy,
-                   prior_a_grand_mu = 1.3,
-                   prior_a_grand_sigma = 0.3,
-                   prior_sigma_a_variety_mu = 2,
-                   prior_sigma_a_variety_sigma = .75,
-                   prior_sigma_a_company_mu = 3,
-                   prior_sigma_a_company_sigma = 1.5,
-                   prior_sigma_y_mu = 1,
-                   prior_sigma_y_sigma = .25) 
+fit <- stan_lmer(ringwidth ~ spp + # main effects
+                   (1 | yr) + (1 | treeid), # random effects
+                 data = wdat,
+                 chains = 2, 
+                 cores = 2, 
+                 iter = 4000, 
+                 warmup=2000,
+                 control = list(adapt_delta = 0.999))
 
-mdl.balanced <- stan("stan/int_variety_company.stan",
-                     data = trait_data,
-                     iter = 3000,
-                     warmup = 2000,
-                     chains = 4,
-                     include = FALSE,
-                     pars = "mu_y",
-                     seed = 202199)## ,
-## control = list(max_treedepth = 15))
+print(fit, digits=2)
 
-
-
-nspp <- 1
 mu_gdd <- 94000 # mean of GDD
 a <- 10 # a for alpha: intercept whatever, I have no clue what to set it at right now
 b <- 0.0001 # b for beta: slope whatever. Assuming, that for each DGG, growth ring rises by that value
 sigma_gdd <- 10 # standard deviation
+
 # Find GDD. For now I'll set it as it starts on DOY 100 and ends on DOY 250. 
 x <- rnorm(n, mu_gdd, sigma_gdd)
 # hist(x)
@@ -132,7 +116,7 @@ fake <- data.frame(x,y)
 # === === === === === === === === === === === === === === === === 
 #### Step 2. Simulate data ####
 # === === === === === === === === === === === === === === === === 
-fit1 <- stan_glm(y~x, data=fake)
+# fit1 <- stan_glm(y~x, data=fake)
 print(fit1, digits=2) # first row shows estimated intercept with its uncertainty. second row shows the estimated slope with its uncertainty. 
 # in the auxiliary parameters, it shows the residual standard deviation sigma with its uncertainty
 
