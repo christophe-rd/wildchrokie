3fa66598ce805db6265d65426f5b8abf93841bfe
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2025-09-11 18:06:07 -0700

    changed sim data code structure and updated posterior recovery from new stan model!

--- analyses/rcode/modelWildchrokie.R
+++ analyses/rcode/modelWildchrokie.R
@@ -20,9 +20,11 @@ library("wesanderson")
 runmodels <- FALSE
 runoldcode <- FALSE
 
-setwd("/Users/christophe_rouleau-desrochers/github/wildchrokie/analyses")
-setwd("/Users/lizzie/Documents/git/projects/others/christophe/wildchrokie/analyses")
-
+if(length(grep("christophe_rouleau-desrochers", getwd()) > 0)) {
+  setwd("/Users/christophe_rouleau-desrochers/github/wildchrokie/analyses")
+} else if(length(grep("lizzie", getwd())) > 0){
+  setwd("/Users/lizzie/Documents/git/projects/wildchrokie/analyses")
+} 
 
 # === === === === === === === === === === === === === === === === 
 #### Step 1. Come up with a model ####
@@ -39,21 +41,21 @@ set.seed(124)
 a <- 1.5
 sigma_y <- 0.2
 sigma_a_spp <- 0.3 # This is pretty low, but I guess you think your species are closely related and will be similar?
-sigma_a_ids <- 0.8
+sigma_a_treeid <- 0.8
 sigma_a_site <- 0.1
 
 n_site <- 4 # number of sites
 n_spp <- 10 # number of species
 n_perspp <- 10 # number of individuals per species
-n_ids <- n_perspp * n_spp * n_site # number of ids
+n_treeid <- n_perspp * n_spp * n_site # number of treeid
 n_meas <- 5 # repeated measurements per id
-N <- n_ids * n_meas # total number of measurements
+N <- n_treeid * n_meas # total number of measurements
 
 
-# get replicated ids
-ids <- rep(rep(rep(1:n_perspp, times = n_spp), times = n_site), each = n_meas)
-# non replicated ids
-idsnonrep <- rep(rep(1:n_perspp, times = n_spp), times = n_site)
+# get replicated treeid
+treeid <- rep(1:n_treeid, each = n_meas)
+# non replicated treeid
+treeidnonrep <- rep(rep(1:n_perspp, times = n_spp), times = n_site)
 # replicated spp
 spp <- rep(rep(rep(1:n_spp, each = n_perspp), times = n_site), each = n_meas) 
 # non replicated spp
@@ -63,30 +65,27 @@ site <- rep(rep(rep(1:n_site, each = n_spp), each = n_perspp), each = n_meas)
 # non replicated site
 site_nonrep <- rep(rep(1:n_site, each = n_spp), each = n_perspp)
 # quick check 
-table(idsnonrep, site_nonrep)
+table(treeidnonrep, site_nonrep)
 
 simcoef <- data.frame(
   site = site,
   spp = spp,
-  ids = ids
+  treeid = treeid
 )
 
 # get 50 intercept values for each species
 a_spp <- rnorm(n_spp, 0, sigma_a_spp)
-
 a_site <- rnorm(n_site, 0, sigma_a_site)
-
-# Option 2
-a_ids <- rnorm(n_ids, 0, sigma_a_ids)
+a_treeid <- rnorm(n_treeid, 0, sigma_a_treeid)
 
 # Add my parameters to the df
 simcoef$a_site <- a_site[simcoef$site]
 simcoef$a_spp <- a_spp[simcoef$spp]
 
-### add index to allow ids to be input at the right place
-id_index <- rep(1:n_ids, each = n_meas)
+### add index to allow treeid to be input at the right place
+# id_index <- rep(1:n_treeid, each = n_meas)
 
-simcoef$a_ids <- a_ids[id_index]
+simcoef$a_treeid <- a_treeid[treeid]
 
 # add the rest of the boring stuff 
 simcoef$a <- 1.5
@@ -99,16 +98,16 @@ simcoef$gddcons <- rnorm(N, 1800, 100)/200
 simcoef$ringwidth <- 
   simcoef$a_site + 
   simcoef$a_spp + 
-  simcoef$a_ids + 
+  simcoef$a_treeid + 
   simcoef$a + 
   (simcoef$b*simcoef$gddcons) + 
   simcoef$error
 
-# prepare grouping factors for stan_lmer (ensure ids are unique within spp)
+# prepare grouping factors for stan_lmer (ensure treeid are unique within spp)
 simcoef$site <- factor(simcoef$site) 
 simcoef$spp <- factor(simcoef$spp)
-simcoef$ids <- factor(simcoef$ids)
-simcoef$ids_uni <- paste(simcoef$site, simcoef$spp, simcoef$ids, sep = "_")
+simcoef$treeid <- factor(simcoef$treeid)
+# simcoef$treeid <- paste(simcoef$site, simcoef$spp, simcoef$treeid, sep = "_")
 
 # === === === === === #
 ##### Run models #####
@@ -121,7 +120,7 @@ if(fitnestedrun) {
     ringwidth ~ 1 + gddcons + 
       (1|site) +
       (1|spp) +
-      (1|ids_uni),
+      (1|treeid),
     data = simcoef,
     chains = 4,
     iter = 4000,
@@ -137,8 +136,8 @@ gdd <- simcoef$gddcons
 Nspp <- length(unique(simcoef$spp))
 Nsite <- length(unique(simcoef$site))
 site <- as.numeric(as.character(simcoef$site))
-species <- as.numeric(as.character(simcoef$sp))
-treeid <- rep(1:length(unique(simcoef$ids_uni)), each=5)
+species <- as.numeric(as.character(simcoef$spp))
+treeid <- treeid
 Ntreeid <- length(unique(treeid))
 table(treeid)
 
@@ -152,37 +151,45 @@ fitpost <- extract(fit)
 # === === === === === === === === === === #
 ##### Recover parameters from the posterior #####
 # === === === === === === === === === === #
-fitnested <- readRDS("output/fitnested")
-df_fit <- as.data.frame(fitnested)
+# fitnested <- readRDS("output/fitnested")
+df_fit <- as.data.frame(fit)
+dim(df_fit)
+summary(fit)
 
-# recover slope
-colnames(df_fit)
-# grab ids nested in spp
-ids_cols <- colnames(df_fit)[grepl("ids_uni", colnames(df_fit))]
-ids_cols <- ids_cols[1:length(ids_cols)-1]
-ids_df <- df_fit[, colnames(df_fit) %in% ids_cols]
+# recover spp
+unique(colnames(df_fit))
+
+# grab treeid nested in spp
+treeid_cols <- colnames(df_fit)[grepl("atreeid", colnames(df_fit))]
+# remove sigma_asp for now
+treeid_cols <- treeid_cols[2:length(treeid_cols)]
+
+treeid_df <- df_fit[, colnames(df_fit) %in% treeid_cols]
 # change their names
-colnames(ids_df) <- sub(".*ids_uni:([^]]+)\\]$", "\\1", colnames(ids_df))
-# empty ids dataframe
-ids_df2 <- data.frame(
-  ids_uni = character(ncol(ids_df)),
-  fit_a_ids = numeric(ncol(ids_df)),  
-  fit_a_ids_per5 = NA, 
-  fit_a_ids_per95 = NA
+colnames(treeid_df) <- sub("atreeid\\[(\\d+)\\]", "\\1", colnames(treeid_df))
+# empty treeid dataframe
+treeid_df2 <- data.frame(
+  treeid = character(ncol(treeid_df)),
+  fit_a_treeid = numeric(ncol(treeid_df)),  
+  fit_a_treeid_per5 = NA, 
+  fit_a_treeid_per95 = NA
 )
-for (i in 1:ncol(ids_df)) { # i = 1
-  ids_df2$ids_uni[i] <- colnames(ids_df)[i]         
-  ids_df2$fit_a_ids[i] <- round(mean(ids_df[[i]]),3)  
-  ids_df2$fit_a_ids_per5[i] <- round(quantile(ids_df[[i]], probs = 0.055), 3)
-  ids_df2$fit_a_ids_per95[i] <- round(quantile(ids_df[[i]], probs = 0.945), 3)
+for (i in 1:ncol(treeid_df)) { # i = 1
+  treeid_df2$treeid[i] <- colnames(treeid_df)[i]         
+  treeid_df2$fit_a_treeid[i] <- round(mean(treeid_df[[i]]),3)  
+  treeid_df2$fit_a_treeid_per5[i] <- round(quantile(treeid_df[[i]], probs = 0.055), 3)
+  treeid_df2$fit_a_treeid_per95[i] <- round(quantile(treeid_df[[i]], probs = 0.945), 3)
 }
-ids_df2
+treeid_df2
 
 # grab spp 
-spp_cols <- colnames(df_fit)[grepl(" spp:", colnames(df_fit))]
+spp_cols <- colnames(df_fit)[grepl("asp", colnames(df_fit))]
+# remove sigma_asp for now
+spp_cols <- spp_cols[2:length(spp_cols)]
+
 spp_df <- df_fit[, colnames(df_fit) %in% spp_cols]
 # change their names
-colnames(spp_df) <- sub(".*spp:([0-9]+).*", "\\1", colnames(spp_df))
+colnames(spp_df) <- sub("asp\\[(\\d+)\\]", "\\1", colnames(spp_df))
 #empty spp df
 spp_df2 <- data.frame(
   spp = character(ncol(spp_df)),
@@ -190,6 +197,7 @@ spp_df2 <- data.frame(
   fit_a_spp_per5 = NA, 
   fit_a_spp_per95 = NA
 )
+spp_df2
 for (i in 1:ncol(spp_df)) { # i = 1
   spp_df2$spp[i] <- colnames(spp_df)[i]         
   spp_df2$fit_a_spp[i] <- round(mean(spp_df[[i]]),3)  
@@ -199,10 +207,13 @@ for (i in 1:ncol(spp_df)) { # i = 1
 spp_df2
 
 # grab site
-site_cols <- colnames(df_fit)[grepl(" site:", colnames(df_fit))]
+site_cols <- colnames(df_fit)[grepl("asite", colnames(df_fit))]
+# remove sigma_asp for now
+site_cols <- site_cols[2:length(site_cols)]
+
 site_df <- df_fit[, colnames(df_fit) %in% site_cols]
 # change their names
-colnames(site_df) <- sub(".*site:([0-9]+).*", "\\1", colnames(site_df))
+colnames(site_df) <- sub("asite\\[(\\d+)\\]", "\\1", colnames(site_df))
 # empty site df
 site_df2 <- data.frame(
   site = character(ncol(site_df)),
@@ -219,43 +230,42 @@ for (i in 1:ncol(site_df)) { # i = 1
 site_df2
 
 # clean and re-arrange in a single df!
-# create ids, spp and site cols
-tmp <- do.call(rbind, strsplit(ids_df2$ids_uni, "_"))
-# re-add the new cols
-ids_df2$site <- as.numeric(tmp[,1])
-ids_df2$spp  <- as.numeric(tmp[,2])
-ids_df2$ids  <- as.numeric(tmp[,3])
-
-# merge spp and ids
-merge1 <- merge(spp_df2, ids_df2, by = "spp")
-merge2 <- merge(merge1, site_df2, by = "site")
-
+# create treeid, spp and site cols
+# tmp <- do.call(rbind, strsplit(treeid_df2$treeid, "_"))
+# # re-add the new cols
+# treeid_df2$site <- as.numeric(tmp[,1])
+# treeid_df2$spp  <- as.numeric(tmp[,2])
+# treeid_df2$treeid  <- as.numeric(tmp[,3])
+# 
+# # merge spp and treeid
+# merge1 <- merge(spp_df2, treeid_df2, by = "spp")
+# merge2 <- merge(merge1, site_df2, by = "site")
+# 
 
 # === === === === === === === #
 # Plot parameter recovery #####
 # === === === === === === === #
 
-# Start with ids ######
-# merge simcoef and model
-idstoplot <- merge(
-  simcoef[!duplicated(simcoef$ids_uni), 
-          c("ids_uni", "a_ids")], 
-                   
-  merge2[!duplicated(merge2$ids_uni), 
-         c("ids_uni", "fit_a_ids", "fit_a_ids_per5", "fit_a_ids_per95")], 
-  by = "ids_uni"
-  )
-
-# plot ids
-a_ids_simXfit_plot <- ggplot(idstoplot, aes(x = a_ids, y = fit_a_ids)) +
+# Start with treeid ######
+# add sim to fit treeid df
+treeidtoplot <- merge(
+  simcoef[!duplicated(simcoef$treeid), 
+          c("treeid", "a_treeid")], 
+  treeid_df2[!duplicated(treeid_df2$treeid), 
+         c("treeid", "fit_a_treeid", "fit_a_treeid_per5", "fit_a_treeid_per95")], 
+  by = "treeid"
+)
+treeidtoplot
+# plot treeid
+a_treeid_simXfit_plot <- ggplot(treeidtoplot, aes(x = a_treeid, y = fit_a_treeid)) +
   geom_point(color = "#046C9A", size = 2) +
-  geom_errorbar(aes(ymin = fit_a_ids_per5, ymax = fit_a_ids_per95), width = 0, color = "darkgray", alpha=0.3) +
+  geom_errorbar(aes(ymin = fit_a_treeid_per5, ymax = fit_a_treeid_per95), width = 0, color = "darkgray", alpha=0.3) +
   geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "#B40F20", linewidth = 1) +
-  labs(x = "sim a_ids", y = "fit a_ids", title = "") +
+  labs(x = "sim a_treeid", y = "fit a_treeid", title = "") +
   theme_minimal()
-a_ids_simXfit_plot
+a_treeid_simXfit_plot
 # ggsave!
-ggsave("figures/a_ids_simXfit_plot.jpeg", a_ids_simXfit_plot, width = 6, height = 6, units = "in", dpi = 300)
+ggsave("figures/a_treeid_simXfit_plot.jpeg", a_treeid_simXfit_plot, width = 6, height = 6, units = "in", dpi = 300)
 
 # === === === === === === === === === === === === === === === === === === === ===
 
@@ -263,7 +273,7 @@ ggsave("figures/a_ids_simXfit_plot.jpeg", a_ids_simXfit_plot, width = 6, height 
 spptoplot <- merge(
   simcoef[!duplicated(simcoef$spp), 
           c("spp", "a_spp")], 
-  merge2[!duplicated(merge2$spp), 
+  spp_df2[!duplicated(spp_df2$spp), 
          c("spp", "fit_a_spp", "fit_a_spp_per5", "fit_a_spp_per95")], 
   by = "spp"
   )
@@ -271,7 +281,7 @@ spptoplot
 
 a_spp_simXfit_plot <- ggplot(spptoplot, aes(x = a_spp, y = fit_a_spp)) +
   geom_point(color = "#046C9A", size = 2) +
-  geom_errorbar(aes(ymin = fit_a_spp_per5, ymax = fit_a_spp_per95), width = 0, color = "darkgray", alpha=0.3) +
+  geom_errorbar(aes(ymin = fit_a_spp_per5, ymax = fit_a_spp_per95), width = 0, color = "darkgray", alpha=0.9) +
   geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "#B40F20", linewidth = 1) +
   labs(x = "sim a_spp", y = "fit a_spp", title = "") +
   theme_minimal()
@@ -283,7 +293,7 @@ ggsave("figures/a_spp_simXfit_plot.jpeg", a_spp_simXfit_plot, width = 6, height 
 sitetoplot <- merge(
   simcoef[!duplicated(simcoef$site), 
           c("site", "a_site")], 
-  merge2[!duplicated(merge2$site), 
+  site_df2[!duplicated(site_df2$site), 
          c("site", "fit_a_site", "fit_a_site_per5", "fit_a_site_per95")], 
   by = "site"
 )
@@ -291,7 +301,7 @@ sitetoplot
 
 a_site_simXfit_plot <- ggplot(sitetoplot, aes(x = a_site, y = fit_a_site)) +
   geom_point(color = "#046C9A", size = 2) +
-  geom_errorbar(aes(ymin = fit_a_site_per5, ymax = fit_a_site_per95), width = 0, color = "darkgray", alpha=0.3) +
+  geom_errorbar(aes(ymin = fit_a_site_per5, ymax = fit_a_site_per95), width = 0, color = "darkgray", alpha=0.9) +
   geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "#B40F20", linewidth = 1) +
   labs(x = "sim a_site", y = "fit a_site", title = "") +
   theme_minimal()
