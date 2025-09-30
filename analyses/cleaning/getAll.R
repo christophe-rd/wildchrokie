c2c6477f014c661d01c178e24694796708d3cd31
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2025-09-29 19:17:27 -0700

    moved around stuff and edited file names

--- analyses/cleaning/getAll.R
+++ analyses/cleaning/getAll.R
@@ -14,31 +14,34 @@ setwd("/Users/christophe_rouleau-desrochers/github/wildchrokie/analyses")
 ### === === === === === === === === === === ###
 # 1. Get the data from the cleaning ring width file: wildchrokie_rw
 source("cleaning/source/cleanRingWidth.R") 
+
 # 2. Get observation data from main repo: obsdata
 source("cleaning/source/cleanObsData.R")
 
-temp <- merge(wildchrokie_rw, obsdata, by = c("id", "spp", "year"))
+# 3. Clean climate datasets: weldhillcomp
+source("cleaning/source/cleanClimate.r") 
+
+# 4. Calculate primary and full growing season GDD
+source("cleaning/source/calculateGrowingSeasonGDD.R")
+
+# merge dfs!
+temp <- merge(wildchrokie_rw, obsdata, by = c("treeid", "spp", "year"))
+
 # get only the years we have data for
 temp2 <- subset(temp, year %in% c(2018, 2019, 2020))
 
-# 3. Get GDD data from main wildhill repo
-source("cleaning/source/combineWeather.R") # may be not needed
-
-# 4. Calculate primary and full growing season GDD
-source("cleaning/source/growingSeasonGDD.R")
-
 temp3 <- merge(temp2,
-               obsdataWithGDD[, c("id", "year", "pgsGDD", "fgsGDD")],
-               by = c("id", "year")
+               obsdataWithGDD[, c("treeid", "year", "pgsGDD", "fgsGDD")],
+               by = c("treeid", "year")
                )
 
 # make some checks
 unique(temp3$sampleType)
-temp4 <- subset(temp3, sampleType != "coresWithCookies")
+# remove 2 samples for a given individuals
+temp4 <- subset(temp3, sampleType != "coresWithCookies") 
 temp5 <- temp4[!is.na(temp4$pgsGDD),]
 temp5$idyear <- paste(temp5$id, temp5$year, sep = "_")
 temp6 <- temp5[!duplicated(temp5$idyear),] 
-hist(temp6$year)
 
 y2020 <- subset(temp4, year == "2020")
 y2020$idyear <- paste(y2020$id, y2020$year, sep = "_")
@@ -46,5 +49,6 @@ y2020 <- y2020[which(!is.na(y2020$leafout) & !is.na(y2020$budset)),]
 y2020 <- y2020[!duplicated(y2020$idyear),]
 nrow(y2020)
 unique(y2020$id)
+
 # merged_df$species<-substr(merged_df$Name, 0,6)
 
