218f4ed87551ee3cb43cb00ba659b4db9487f489
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2026-07-29 12:48:50 -0700

    added daymet in clean climate but will have to fix as it doesnt curreently run

--- analyses/cleaning/source/cleanClimate.r
+++ analyses/cleaning/source/cleanClimate.r
@@ -6,6 +6,7 @@
 library(ggplot2)
 # library(wesanderson)
 library(tidyverse)
+library(daymetr)
 
 setwd("/Users/christophe_rouleau-desrochers/github/wildchrokie/analyses/input/_notcookies/")
 
@@ -301,3 +302,48 @@ write_csv(merged, "output/climateSummariesYear.csv")
 terra2 <- subset(terra, year >2015 & year < 2025)
 terra2 <- terra2[, 1:ncol(terra2)-1]
 write_csv(terra2, "output/climateSummariesByMonth.csv")
+
+# <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+# Daymet data
+# <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
+data <- list()
+
+coords <- data.frame(ID = seq(1,4), 
+                     lat = c(42.5315, 45.9310, 44.92466697, 43.99837498),
+                     long = c(-72.1900, -74.0248, -71.09543597, -71.37388296))
+leap <- seq(1960, 1980, 4)
+daymet <- download_daymet("daymet", 42.5315, -72.1900, 1980, 2023)
+
+for(n in 1:nrow(coords)){
+  daymet <- download_daymet("daymet", coords$lat[n], coords$long[n], 1960, 1980)
+  temp <- subset(daymet$data, select = c("year", "yday", "tmax..deg.c.", "tmin..deg.c."))
+  
+  for(y in leap){
+    tmin_366 <- mean(temp$tmax..deg.c.[temp$year == y & temp$yday == 365],
+                     temp$tmax..deg.c.[temp$year == (y + 1) & temp$yday == 1])
+    tmax_366 <- mean(temp$tmin..deg.c.[temp$year == y & temp$yday == 365],
+                     temp$tmin..deg.c.[temp$year == (y + 1) & temp$yday == 1])
+    
+    yday_366 <- data.frame(year = y, yday = 366,
+                           tmax..deg.c. = tmax_366, tmin..deg.c. = tmin_366)
+    
+    temp <- rbind(temp, yday_366)
+  }
+  
+  colnames(temp) <- c("year", "doy", "tmax", "tmin")
+  temp$siteID <- coords$ID[n]
+  temp <- temp[,c("siteID", "year", "doy", "tmax", "tmin")]
+  
+  data[[n]] <- temp
+}
+data <- do.call(rbind, data)
+data <- data[order(data$siteID, data$year, data$doy),]
+
+write.csv(coords, "sites.csv", row.names = FALSE)
+write.csv(data, "temps.csv", row.names = FALSE)
+
+colnames(temps)[colnames(temps) == "siteID"] <- "ID"
+
+dayMet <- merge(coords, temps, by = "ID")
+
+write.csv(dayMet, "analyses/daymetData/dayMet.csv", row.names = F)
