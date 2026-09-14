3385481201a23d33da8c64ac8fca08dfc2c9dfc6
christophe-rd <110614880+christophe-rd@users.noreply.github.com>
2026-09-14 11:22:21 -0400

    fixed problem in clean climate and removed daymet from that script

--- analyses/cleaning/source/cleanClimate.r
+++ analyses/cleaning/source/cleanClimate.r
@@ -65,7 +65,7 @@ yr15TO19_3$pptMM <- yr15TO19_ppt$pptMM[match(yr15TO19_3$date, yr15TO19_ppt$date)
 # Then follow with years 2020 to 2024
 # From https://newa.cornell.edu/all-weather-data-query/
 # precipitation in inches
-files <- list.files(pattern = "\\.csv$")[2:6] # 2 and 10 is wrong
+files <- list.files(pattern = "\\.csv$")[3:7] # 2 and 10 is wrong
 
 # Read them all in as a list of data.frames
 df_list <- lapply(files, read.csv, stringsAsFactors = FALSE)
@@ -303,47 +303,3 @@ terra2 <- subset(terra, year >2015 & year < 2025)
 terra2 <- terra2[, 1:ncol(terra2)-1]
 write_csv(terra2, "output/climateSummariesByMonth.csv")
 
-# <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
-# Daymet data
-# <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
-data <- list()
-
-coords <- data.frame(ID = seq(1,4), 
-                     lat = c(42.5315, 45.9310, 44.92466697, 43.99837498),
-                     long = c(-72.1900, -74.0248, -71.09543597, -71.37388296))
-leap <- seq(1960, 1980, 4)
-daymet <- download_daymet("daymet", 42.5315, -72.1900, 1980, 2023)
-
-for(n in 1:nrow(coords)){
-  daymet <- download_daymet("daymet", coords$lat[n], coords$long[n], 1960, 1980)
-  temp <- subset(daymet$data, select = c("year", "yday", "tmax..deg.c.", "tmin..deg.c."))
-  
-  for(y in leap){
-    tmin_366 <- mean(temp$tmax..deg.c.[temp$year == y & temp$yday == 365],
-                     temp$tmax..deg.c.[temp$year == (y + 1) & temp$yday == 1])
-    tmax_366 <- mean(temp$tmin..deg.c.[temp$year == y & temp$yday == 365],
-                     temp$tmin..deg.c.[temp$year == (y + 1) & temp$yday == 1])
-    
-    yday_366 <- data.frame(year = y, yday = 366,
-                           tmax..deg.c. = tmax_366, tmin..deg.c. = tmin_366)
-    
-    temp <- rbind(temp, yday_366)
-  }
-  
-  colnames(temp) <- c("year", "doy", "tmax", "tmin")
-  temp$siteID <- coords$ID[n]
-  temp <- temp[,c("siteID", "year", "doy", "tmax", "tmin")]
-  
-  data[[n]] <- temp
-}
-data <- do.call(rbind, data)
-data <- data[order(data$siteID, data$year, data$doy),]
-
-write.csv(coords, "sites.csv", row.names = FALSE)
-write.csv(data, "temps.csv", row.names = FALSE)
-
-colnames(temps)[colnames(temps) == "siteID"] <- "ID"
-
-dayMet <- merge(coords, temps, by = "ID")
-
-write.csv(dayMet, "analyses/daymetData/dayMet.csv", row.names = F)
